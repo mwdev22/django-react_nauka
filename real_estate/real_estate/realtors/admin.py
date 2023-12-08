727be824a977d4fe018976d81a58ad33ebe86432
maciek <92087346+mwdev22@users.noreply.github.com>
2023-12-08 18:38:09 +0100

    Delete real_estate directory

--- real_estate/real_estate/realtors/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Realtor
-
-class RealtorAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'email', 'date_hired')
-    list_display_links = ('id', 'name')
-    search_fields = ('name', )
-
-admin.site.register(Realtor, RealtorAdmin)
--- real_estate/real_estate/realtors/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.RealtorListView.as_view()),
-    path('top_seller', views.TopSellerView.as_view()),
-    path('<int:pk>', views.RealtorView.as_view())
-]
